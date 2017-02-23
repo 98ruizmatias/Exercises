2b21f4fbf3b8e4255c0720e861ca387d02e04743
Matias Ruiz <matias.alejandro.ruiz@hotmail.com>
2017-02-23 10:28:35 -0500

    Ejercicio 1

---
+++ Ejercicios.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int x, y, base, exp;
+
+int main(int argc, char const *argv[])
+{
+	printf("Ingrese la base: ");
+	scanf("%d", &x);
+	printf("Ingrese el exponente: ");
+	scanf("%d", &y);
+	base = x;
+	exp = y;
+	for (int i = 0; i < exp; ++i)
+	{
+		base = base * x;
+	}
+	printf("El resultado de la potencia es: %c\n", base);
+	system("PAUSE")
+	return 0;
+}
